4ca4ce8628fe025794d69b4770e6473ab4ee25a1
djf <488698112@qq.com>
2018-11-20 17:55:00 +0800

    导包异常修复

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wanandroid/main.dart';
+import 'package:wanAndroid/pages/WanAndroidPage.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
